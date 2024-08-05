4bfd2889b605ea05cee2a3b8aefa1b73b19836ce
Akshay-Daundkar-14 <akshaydaundkar2000@gmail.com>
2024-08-05 11:07:15 +0530

    Basic Routing

---
+++ src/pages/coaches/CoachDetails.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Coach Details</h1>
+    <router-view></router-view>
+    <router-link to="/coaches/c3/contact">Contact Coach</router-link>
+</template>
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>FILTER</section>
+
+
+    <div class="controls">
+        <button>Refresh</button>
+        <router-link to="/register">Register as Coach</router-link>
+    </div>
+
+  <section>
+    <ul>LIST OF COACHES</ul>
+  </section>
+</template>
